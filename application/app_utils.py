ebe7f3636616c0f5c557ba3bdbcd1bebd2ccc0a5
Tomasz Tomaka <ttomaka@pgs-soft.com>
2020-04-28 15:46:12 +0200

    Make AppUtils.prefix_route static mehod

--- application/app_utils.py
+++ application/app_utils.py
@@ -1,4 +1,5 @@
-class AppUtils():
+class AppUtils:
+    @staticmethod
     def prefix_route(route_function, prefix='', mask='{0}{1}'):
         def newroute(route, *args, **kwargs):
             return route_function(mask.format(prefix, route), *args, **kwargs)
