7a0cd1cf8ebe5b05fbec8b1b905995ce3ac40717
Tomasz Tomaka <ttomaka@pgs-soft.com>
2020-04-30 16:46:39 +0200

    Use uuid4 instead of uuid1

--- application/s3_transfer.py
+++ application/s3_transfer.py
@@ -14,7 +14,7 @@ def upload_file_to_s3(cv_url=None):
     s3 = session.resource('s3')
     bucket_name = Config.BUCKET_NAME
 
-    s3_file_key = str(uuid.uuid1()) + '.pdf'
+    s3_file_key = str(uuid.uuid4()) + '.pdf'
     bucket = s3.Bucket(bucket_name)
     bucket.upload_fileobj(rq.raw, s3_file_key)
     return f'https://{bucket_name}.s3.amazonaws.com/{s3_file_key}'
